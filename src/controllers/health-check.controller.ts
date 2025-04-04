482cb855749efd2a672ec94c3bbeea704f068f63
Ian Santos <contato.iansantos@gmail.com>
2025-04-03 22:57:44 -0400

    feat: add Swagger documentation and health check endpoint

--- src/controllers/health-check.controller.ts
+++ src/controllers/health-check.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheckService } from 'src/use-cases/health-check.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('Health Check')
 @Controller()
 export class HealthCheckController {
   constructor(private readonly healthCheckService: HealthCheckService) {}
 
+  @ApiOperation({
+    summary: 'Check the health of the application',
+    description: 'Returns the health status of the application.',
+  })
   @Get()
+  getHealthStatus(): { status: number; message: string } {
+    return { status: 200, message: this.healthCheckService.getHealthCheck() };
+  }
   getHello(): string {
     return this.healthCheckService.getHealthCheck();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Scheduling API')
+    .setDescription(
+      'API for registering students and teachers and scheduling classes.',
+    )
+    .setContact(
+      'Ian Santos',
+      'https://github.com/IanDeSa',
+      'contato.iansantos@gmail.com',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT || 3001;
   await app.listen(port);
 }
